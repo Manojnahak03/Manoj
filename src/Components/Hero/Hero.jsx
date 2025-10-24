ac89587e3a5e9aa6c735a4ac96e0a143543915f0
Manojnahak03 <45Manojnahak@gmail.com>
2025-10-24 21:16:19 +0530

    Testing Resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import './Hero.css';
 import Profile_img from '../../assets/Profile_img.jpg';
-import Resume from '../../assets/ManojResume.pdf'; 
+
 const Hero = () => {
   return (
     <div id='home' className='hero'>
       <img src={Profile_img} alt="Profile" />
       <h1><span>I'm Manoj Nahak,</span> frontend developer based in India.</h1>
-      <p>I am a frontend developer from Mumbai, India with 2 years of experience in Microsoft, Google, and Tesla.</p>
-      
+      <p>
+        I am a frontend developer from Mumbai, India with 2 years of experience in Microsoft, Google, and Tesla.
+      </p>
+
       <div className="hero-action">
         <div className="hero-connect">Connect with me</div>
 
         {/* Resume Button */}
-        <a href={Resume} target="_blank" rel="noopener noreferrer" className="hero-resume">
+        <a
+          href="/ManojResume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hero-resume"
+        >
           My Resume
         </a>
       </div>
