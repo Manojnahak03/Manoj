c7e07ed4563b8082edbafc5642ab8b22602254a9
Manojnahak03 <45Manojnahak@gmail.com>
2025-10-24 15:41:11 +0530

    Mobile Responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.svg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React, { useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.svg';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { FaBars, FaTimes } from 'react-icons/fa'; // for hamburger & close icons
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className='navbar'>
-      <img src={logo} alt="Logo" />
+    <div className="navbar">
+      <img src={logo} alt="Logo" className="nav-logo" />
 
-      <ul className="nav-menu">
-        <li><AnchorLink href="#home">Home</AnchorLink></li>
-        <li><AnchorLink href="#about">About Me</AnchorLink></li>
-        <li><AnchorLink href="#service">Services</AnchorLink></li>
-        <li><AnchorLink href="#work">Portfolio</AnchorLink></li>
-        <li><AnchorLink href="#contact">Contact</AnchorLink></li>
-      </ul>
+      <div className={`nav-menu ${isOpen ? 'open' : ''}`}>
+        <li><AnchorLink href="#home" onClick={closeMenu}>Home</AnchorLink></li>
+        <li><AnchorLink href="#about" onClick={closeMenu}>About Me</AnchorLink></li>
+        <li><AnchorLink href="#service" onClick={closeMenu}>Services</AnchorLink></li>
+        <li><AnchorLink href="#work" onClick={closeMenu}>Portfolio</AnchorLink></li>
+        <li><AnchorLink href="#contact" onClick={closeMenu}>Contact</AnchorLink></li>
+      </div>
 
-      <div className="nav-connect"><AnchorLink href="#contact">Connect With Me</AnchorLink></div>
+      <div className="nav-connect">
+        <AnchorLink href="#contact">Connect With Me</AnchorLink>
+      </div>
+
+      {/* Hamburger Icon */}
+      <div className="hamburger" onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
